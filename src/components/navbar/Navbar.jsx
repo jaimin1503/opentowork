f799a9f892bd105bce786a174f637dd3eab5ddf8
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2023-10-01 17:40:29 +0530

    responsive navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,38 @@
 import './style.css'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom';
+import './script.js'
 
 export default function Navbar() {
 	const navigat=useNavigate();
 	const signuphandler=()=>{
 		navigat("/welcome")
 	}
+	const navbar = document.querySelector('.navbar')
+	const ula = document.querySelector('.ula')
+	const handleClick = () => {
+		navbar.classList.toggle('h_nav_resp')
+		ula.classList.toggle('v_nav_resp')
+	}
   return (
-	<div className="flex  justify-between gradient_home">
-		<div className="left_side w-[60vw]">
-			<ul className=" flex justify-around border-b-2 py-3 border-purple-600">
-				<Link to="/" className=" cursor-pointer"><img src="#" alt="sdfg" /></Link>
-				<Link to='/DevIT' className=" text-white cursor-pointer hover:text-purple-200">Development & IT</Link>
-				<Link to='/AI' className=" text-white cursor-pointer hover:text-purple-200">AI Services</Link>
-				<Link to='/Design' className=" text-white cursor-pointer hover:text-purple-200">Design & Creation</Link>
-				<li className=" cursor-pointer text-white hover:text-purple-200">Sales & Marketing</li>
-				<li className=" cursor-pointer text-white hover:text-purple-200">Writing & more</li>
+	<>
+	<div onClick={handleClick} class="burger" id="burger">
+			<div class="line"></div>
+			<div class="line"></div>
+			<div class="line"></div>
+		</div>
+	<div className="navbar h_nav_resp flex  justify-between gradient_home border-b-2 border-purple-600" style={{transition:'0.5s'}}>
+		<div className="left_side w-[60vw] ">
+			<ul className="ula v_nav_resp flex justify-around py-3">
+				<Link to="/" className=" li cursor-pointer"><img src="#" alt="sdfg" /></Link>
+				<Link to='/DevIT' className=" li text-white cursor-pointer hover:text-purple-200">Development & IT</Link>
+				<Link to='/AI' className=" li text-white cursor-pointer hover:text-purple-200">AI Services</Link>
+				<Link to='/Design' className=" li text-white cursor-pointer hover:text-purple-200">Design & Creation</Link>
+				<Link to='/' className=" li cursor-pointer text-white hover:text-purple-200">Sales & Marketing</Link>
+				<Link to='/' className=" li cursor-pointer text-white hover:text-purple-200">Writing & more</Link>
 			</ul>
 		</div>
-		<div className="right_side w-[40vw] flex border-b-2 justify-end px-5 items-center border-purple-600">
+		<div className="right_side v_nav_resp w-[40vw] flex justify-end px-5 items-center">
 			<button className="log_in" >
 				<p className=" text-white cursor-pointer px-5 hover:text-purple-200">log in</p>
 			</button>
@@ -29,5 +42,6 @@ export default function Navbar() {
 		</div>
 		
 	</div>
+	</>
   )
 }
---
+++ src/components/navbar/script.js
@@ -0,0 +1,13 @@
+// let burger = document.querySelector(".burger");
+// let navbar = document.querySelector(".navbar");
+// let ula = document.querySelector(".ula")
+
+// burger.addEventListener("click", () => {
+//   navbar.classList.toggle("h_nav_resp");
+//   ula.classList.toggle("v_nav_resp");
+// });
+
+// burger.addEventListener('click', () => {
+// 	// navbar.classList.toggle('h_nav_resp')
+// 	// ula.classList.toggle('v_nav_resp')
+// })
