9c8eac81e3b795550890fda37bfb858fea8388fe
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2023-10-06 14:14:44 +0530

    dev page css

--- src/components/D_IT/DevIT.jsx
+++ src/components/D_IT/DevIT.jsx
@@ -28,83 +28,83 @@ export default function DevIT() {
 	};
   return (
 	<div className='gradient_dev'>
-		<div className="row1 py-10 flex items-center ml-16">
-			<div className="row1imagearea w-[90vw] h-[70vh] bg-[#4a426a] rounded-xl flex justify-between">
-				<div className="textarea w-[40%] flex items-center justify-center flex-col ">
-					<h1 className='row1_text text-5xl mx-5 my-4 text-white max-w-[80%]'>Developers and IT experts to scale your org.</h1>
-					<h2 className='row1_text text-xl mx-5 my-4 text-gray-400 max-w-[80%]'>Hire the particular experts and get your work done within your timeline.</h2>
+		<div className="row1">
+			<div className="row1imagearea">
+				<div className="textarea">
+					<h1 className='row1_text'>Developers and IT experts to scale your org.</h1>
+					<h2 className='row1_text'>Hire the particular experts and get your work done within your timeline.</h2>
 				</div>
-				<div className="imagearea w-[60%] flex items-center">
-					<img className=' rounded-xl object-cover h-[95%] ml-10' src={solodev} alt="developer" />
+				<div className="imagearea">
+					<img className='rounded-xl' src={solodev} alt="developer" />
 				</div>
 			</div>
 		</div>
-		<div className="row2  flex flex-col items-center justify-center py-10">
-			<h1 className=' text-6xl font-extrabold text-white my-10' >
+		<div className="row2">
+			<h1>
 				Find the best Developers and IT experts
 			</h1>
-			<div className="types flex ">
-				<div className="type1 flex justify-center items-center w-[20vw] h-[20vh] bg-[#4a426a] rounded-xl mx-5 my-5">
-					<img className='h-[80%] w-[80%] object-contain hover:scale-105' style={{transition:'0.3s'}} src={web} alt="da" />
+			<div className="types">
+				<div className="type1">
+					<img style={{transition:'0.3s'}} src={web} alt="da" />
 				</div>
-				<div className="type1 flex justify-center items-center w-[20vw] h-[20vh] bg-[#4a426a] rounded-xl mx-5 my-5">
-					<img className='h-[80%] w-[80%] object-contain hover:scale-105' style={{transition:'0.3s'}} src={react} alt="dd" />
+				<div className="type1">
+					<img style={{transition:'0.3s'}} src={react} alt="dd" />
 				</div>
-				<div className="type1 flex justify-center items-center w-[20vw] h-[20vh] bg-[#4a426a] rounded-xl mx-5 my-5">
-					<img className='h-[80%] w-[80%] object-contain hover:scale-105' style={{transition:'0.3s'}} src={ruby} alt="dd" />
+				<div className="type1">
+					<img style={{transition:'0.3s'}} src={ruby} alt="dd" />
 				</div>
-				<div className="type1 flex justify-center items-center w-[20vw] h-[20vh] bg-[#4a426a] rounded-xl mx-5 my-5">
-					<img className='h-[80%] w-[80%] object-contain hover:scale-105' style={{transition:'0.3s'}} src={nodejs} alt="ds" />
+				<div className="type1">
+					<img style={{transition:'0.3s'}} src={nodejs} alt="ds" />
 				</div>
 			</div>
 			<div className="types flex justify-between">	
-				<div className="type1 flex justify-center items-center w-[20vw] h-[20vh] bg-[#4a426a] rounded-xl mx-5 my-5">
-					<img className='h-[80%] w-[80%] object-contain hover:scale-105' style={{transition:'0.3s'}} src={mysql} alt="sd" />
+				<div className="type1">
+					<img style={{transition:'0.3s'}} src={mysql} alt="sd" />
 				</div>
-				<div className="type1 flex justify-center items-center w-[20vw] h-[20vh] bg-[#4a426a] rounded-xl mx-5 my-5">
-					<img className='h-[80%] w-[80%] object-contain hover:scale-105' style={{transition:'0.3s'}} src={python} alt="dsd" />
+				<div className="type1">
+					<img style={{transition:'0.3s'}} src={python} alt="dsd" />
 				</div>
-				<div className="type1 flex justify-center items-center w-[20vw] h-[20vh] bg-[#4a426a] rounded-xl mx-5 my-5">
-					<img className='h-[80%] w-[80%] object-contain hover:scale-105' style={{transition:'0.3s'}} src={cpp} alt="sd" />
+				<div className="type1">
+					<img style={{transition:'0.3s'}} src={cpp} alt="sd" />
 				</div>
-				<div className="type1 flex justify-center items-center w-[20vw] h-[20vh] bg-[#4a426a] rounded-xl mx-5 my-5">
-					<img className='h-[80%] w-[80%] object-contain hover:scale-105' style={{transition:'0.3s'}} src={java} alt="dsd" />
+				<div className="type1">
+					<img style={{transition:'0.3s'}} src={java} alt="dsd" />
 				</div>
 			</div>
 		</div>
 		<div className="row3">
-			<div className="text flex justify-center py-10">
-				<h1 className='text-white text-6xl font-extrabold my-5'>
+			<div className="text">
+				<h1>
 					Development and IT projects for all yoour need.
 				</h1>
 			</div>
-			<div className="imageandtext flex justify-center">
-				<div className="text2 h-[50vh] w-[90vw] rounded-xl my-10 bg-[#4a426a] flex">
-					<ul className='flex-col flex w-[30%] pt-10 pb-5 pl-10'>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(app)}>Mobile app Development</li>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(shopify)}>Shopify Development</li>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(workpress)}>WordPress Development</li>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(data)}>Data Visualization</li>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(ml)}>Machine Learning</li>
-					<button className='bg-purple-500 mr-10 mt-10 text-white hover:bg-purple-600 rounded-full py-2 px-4'>Browse Projects</button>
+			<div className="imageandtext">
+				<div className="text2">
+					<ul>
+						<li className='li' onClick={() => changeImage(app)}>Mobile app Development</li>
+						<li className='li' onClick={() => changeImage(shopify)}>Shopify Development</li>
+						<li className='li' onClick={() => changeImage(workpress)}>WordPress Development</li>
+						<li className='li' onClick={() => changeImage(data)}>Data Visualization</li>
+						<li className='li' onClick={() => changeImage(ml)}>Machine Learning</li>
+					<button className='bg-purple-500'>Browse Projects</button>
 					</ul>
 				<div className="image w-[70%]">
-					<img className='w-[100%] h-[100%] object-cover rounded-xl' src={currentImage} alt="dfg" />
+					<img src={currentImage} alt="dfg" />
 				</div>
 				</div>
 			</div>
 		</div>
-		<div className="row">
-			<div className="button flex justify-center py-10">
-				<button className="bg-purple-500 py-3 px-10 text-white rounded-full hover:scale-105" style={{transition:'0.3s'}}>Join Now</button>
+		<div className="row4">
+			<div className="button">
+				<button>Join Now</button>
 			</div>
-			<div className="row3-photos flex justify-center">
-				<img className=' w-[30vw] my-5 mx-5 mt-20 h-[40vh] rounded-xl ' src={display1} alt="img" />
-				<img className=' w-[30vw] my-5 mx-5 h-[40vh] rounded-xl ' src={display2} alt="img" />
+			<div className="row3-photos">
+				<img src={display1} alt="img" />
+				<img src={display2} alt="img" />
 			</div>
-			<div className="row3-photos flex justify-center">
-				<img className=' w-[30vw] my-5 mx-5 mt-20 h-[40vh] rounded-xl ' src={display3} alt="img" />
-				<img className=' w-[30vw] my-5 mx-5 h-[40vh] rounded-xl ' src={display4} alt="img" />
+			<div className="row3-photos">
+				<img src={display3} alt="img" />
+				<img src={display4} alt="img" />
 			</div>
 		</div>
 	</div>
