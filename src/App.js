7a344719b830a86523fa6aa756fa4917c7086a16
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2023-10-22 14:30:48 +0530

    basic styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from "./components/navbar/Navbar";
 import Welcome from "./components/welcome/Welcome";
 import {Routes,Route} from 'react-router-dom'
 import Design from "./components/design/Design";
-import Display from "./components/display/Display";
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
       <Route path="/Design" element={<Design/>}></Route>
       </Routes>
       <Footer/>
-      <Display/>
     </div>
   );
 }
--- src/components/design/Design.jsx
+++ src/components/design/Design.jsx
@@ -5,42 +5,31 @@ import brand from './assets/brand.jpg'
 import design from './assets/designing.jpg'
 import photoshop from './assets/photoshop.jpg'
 import presentation from './assets/presentation.jpg'
-import videoedit from './assets/videoedit.jpg'
-import { useState } from 'react'
 import photography from './assets/photography.jpg'
 import video from './assets/videoediting.mp4'
+import Display from '../display/Display'
+import ChangeImage from '../changeImage/ChangeImage'
 
 export default function DevIT() {
-	const [currentImage, setCurrentImage] = useState(brand);
 
-	// Define a function to handle image changes
-	const changeImage = (newImage) => {
-	  setCurrentImage(newImage);
-	};
   return (
 	<div className='gradient_dev'>
-		<div className="design-row-1 mx-16 flex justify-center  py-10">
-			<div id='roww1' className="row1imagearea w-[90vw] h-[70vh] bg-[#4a426a] rounded-xl flex">
-				<div className="textarea w-[40%] flex items-center justify-center flex-col ">
-					<h1 className=' text-5xl mx-5 my-4 text-white max-w-[80%] font-bold'>Best Designers and Creators for your business.</h1>
-					<h2 className=' text-xl mx-5 my-4 text-gray-400 max-w-[80%]'>Find the best design that suits your work and personality.</h2>
-				</div>
-				<div className="imagearea w-[60%] flex items-center mr-3">
-					<img className=' rounded-xl object-cover h-[95%] ' src={interior} alt="interior" />
-				</div>
-			</div>
-		</div>
+		<Display 
+			heading1 = "Best Designers and Creators for your business."
+			heading2 = "Find the best design that suits your work and personality."
+			url = {interior}
+		/>
 		<div className="row">
 			
-			<div className="row3-photos flex justify-center items-center pb-10">
-				<img className=' h-[50vh] w-[70vw] object-cover my-5 mx-5 mt-20 rounded-xl ' src={photography} alt="img" />
+			<div className="row3-photos">
+				<img src={photography} alt="img" />
 				<h1 className=' text-white text-5xl font-bold mx-10'>Photography</h1>
 
 			</div>
 			<hr className='mb-5' />
-			<div className="row3-photos flex justify-center items-center py-10">
-				<h1 className=' text-white text-5xl font-bold mx-10'>Video Editing</h1>
-				<video autoPlay className='w-[70vw] h-[50vh]' muted controls>
+			<div className="row3-photos">
+				<h1>Video Editing</h1>
+				<video autoPlay muted controls>
 					<source  src={video} type="video/mp4"  />
 				</video>
 
@@ -52,22 +41,19 @@ export default function DevIT() {
 				Design and creative projects for your most pressing work.
 				</h1>
 			</div>
-			<div className="imageandtext flex justify-center">
-				<div className="text2 h-[50vh] w-[90vw] rounded-xl my-10 bg-[#4a426a] flex">
-					<ul className='flex-col flex w-[30%] pt-10 pb-5 pl-10'>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(brand)}>Logo Design</li>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(photoshop)}>Photoshop Editing</li>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(design)}>Web & Mobile Design</li>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(presentation)}>Presentation Design</li>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(house)}>Architecture & Interior Design</li>
-						<li className=' text-white text-xl py-2 active:text-purple-400 hover:text-purple-500 cursor-pointer' onClick={() => changeImage(videoedit)}>Video Editing</li>
-					<button className='bg-purple-500 mr-10 mt-10 text-white hover:bg-purple-600 rounded-full py-2 px-4'>Browse Projects</button>
-					</ul>
-				<div className="image w-[70%]">
-					<img className='w-[100%] h-[100%] object-cover rounded-xl' src={currentImage} alt="dfg" />
-				</div>
-				</div>
-			</div>
+			<ChangeImage
+				initial = {brand}
+				text1 = "Logo Design"
+				text2 = "Photoshop Editing"
+				text3 = "Web & Mobile Design"
+				text4 = "Presentation Design"
+				text5 = "Architecture & Interior Design"
+				url1 = {brand}
+				url2 = {photoshop}
+				url3 = {design}
+				url4 = {presentation}
+				url5 = {house}
+			/>
 		</div>
 		<div className="button flex justify-center py-10">
 				<button className="bg-purple-500 py-3 px-10 text-white rounded-full hover:scale-105" style={{transition:'0.3s'}}>Join Now</button>
