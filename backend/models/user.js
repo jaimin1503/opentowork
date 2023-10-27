0d25cc170057da8956b940f3c0a29bf8fd21ef31
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2023-10-27 07:06:36 +0530

    start of user login

---
+++ backend/models/user.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: "string",
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: "string",
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+  },
+  first_name: {
+    type: "string",
+    required: true,
+  },
+  last_name: {
+    type: "string",
+    required: true,
+  },
+  profile_picture: {
+    type: "string",
+    default: "default_profile_picture.jpg",
+  },
+  description: {
+    type: "string",
+    default: "",
+  },
+  skills: {
+    type: "array",
+    items: {
+      type: "string",
+    },
+  },
+  location: {
+    type: "string",
+    default: "India",
+  },
+  hourly_rate: {
+    type: "number",
+    default: 0,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
