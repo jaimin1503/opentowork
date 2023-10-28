42a648238312ef5fc35e1959cd0d34f208e87639
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2023-10-28 07:01:15 +0530

    Update AllPosts.jsx

--- src/components/allposts/AllPosts.jsx
+++ src/components/allposts/AllPosts.jsx
@@ -27,7 +27,27 @@ export default function AllPosts() {
           {posts.map((post) => {
             return (
               <div key={post._id}>
-                <h1>${post.title}</h1>
+                <div className=" w-[60vw] h-[35vh] border-2 rounded-xl border-purple-300 m-auto mt-5">
+                  <div className=" p-5 text-xl font-bold tracking-wider">
+                    -{post.title}
+                  </div>
+                  <div className=" p-5">{post.description}</div>
+                  <div className="m-5 flex">
+                    {post.skillsRequired.map((skill) => {
+                      return (
+                        <div className="mx-2">
+                          <div className="px-5 border-2 rounded-full border-purple-200 bg-purple-100 w-24 flex justify-center items-center">
+                            {skill}
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="p-5">
+                    <div className="bodget">Rs. {post.budget}</div>
+                    <div className="deadline"></div>
+                  </div>
+                </div>
               </div>
             );
           })}
