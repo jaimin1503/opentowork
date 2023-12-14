f042bbf1089093ec5d70d7d822e77a6c9796cfba
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2023-12-14 11:23:48 +0530

    responsive navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,28 +17,55 @@ export default function Navbar() {
   };
   return (
     <>
+      {!visible ? (
+        <Link to="/" className="">
+          <img
+            className=" absolute m-2 h-[35px] w-[35px]"
+            src={logo}
+            alt="sdfg"
+          />
+        </Link>
+      ) : (
+        ""
+      )}
       <div onClick={handleClick} className="burger" id="burger">
         <div className="line"></div>
         <div className="line"></div>
         <div className="line"></div>
       </div>
-      <div className={`navbar ${isOpen ? "h_nav_resp" : ""} gradient_home`}>
-        <div className="left_side">
-          <ul className={`ula ${!visible ? "v_nav_resp" : ""}`}>
-            <Link to="/" className=" li hover:text-purple-500">
-              <img className="logo" src={logo} alt="sdfg" />
+      <div
+        className={`navbar ${isOpen ? "h_nav_resp" : ""} flex justify-between`}
+      >
+        <div className="left_side text-xl w-[60%] sm:w-[50%] lg:w-[45%]">
+          <ul
+            className={`ula ${
+              !visible ? "v_nav_resp" : ""
+            } flex justify-between`}
+          >
+            <Link to="/" className=" p-2 ml-5">
+              <img className="logo h-[35px] w-[35px]" src={logo} alt="sdfg" />
             </Link>
-            <Link to="/DevIT" className=" li hover:text-purple-500">
+            <Link
+              to="/DevIT"
+              className=" li text-purple-200 hover:text-purple-500"
+            >
               Development & IT
             </Link>
-            <Link to="/Design" className=" li hover:text-purple-500">
+            <Link
+              to="/Design"
+              className=" li text-purple-200 hover:text-purple-500"
+            >
               Design & Creation
             </Link>
           </ul>
         </div>
-        <div className={`right_side ${!visible ? "v_nav_resp" : ""}`}>
-          <div className="log_in">
-            <li className=" text-white group cursor-pointer list-none px-5 hover:text-purple-500">
+        <div
+          className={`right_side ${
+            !visible ? "v_nav_resp" : ""
+          } flex items-center mr-5`}
+        >
+          <div className="log_in text-xl">
+            <li className=" text-purple-200 group cursor-pointer list-none px-5 hover:text-purple-500">
               log in
               <ul className=" absolute hidden rounded-lg group-hover:block bg-purple-800 py-2 px-8 space-y-1">
                 <Link to="/Login">
