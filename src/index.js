bf145b087868e7d0b39979a2fcb4af6d5c784ffd
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-03-26 09:26:31 +0530

    cleaning

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -13,5 +12,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
